ba15c402d515b5ceeae181daf63b9c36e94862c8
Unknown <mehm0639@stud.kea.dk>
2019-05-10 13:54:34 +0200

    Customer and car class

---
+++ KF/KF/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KF.Models
+{
+    public class Customer
+    {
+        public int customerId { get; set; }
+        public int cprNr { get; set; }
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String adress { get; set; }
+        public int zippCode { get; set; }
+        public String city { get; set; }
+        public int phoneNumber { get; set; }
+        public String mail { get; set; }
+        public String customerType { get; set; }
+    }
+}
