dc39bc7e03c2c46baea940ad43553a429d53239a
Unknown <mehm0639@stud.kea.dk>
2019-05-14 14:52:58 +0200

    Model in KFWCF

---
+++ KFWCFServiceLibrary/KFWCFServiceLibrary/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Serialization;
+
+namespace KFWCFServiceLibrary.Models
+{
+    public class Customer
+    {
+        [DataMember]
+        public int CustomerId { get; set; }
+        [DataMember]
+        public int CprNr { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public int Zipcode { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public int PhoneNumber { get; set; }
+        [DataMember]
+        public string Mail { get; set; }
+        [DataMember]
+        public string CustomerType { get; set; }
+        [DataMember]
+        public int Seniority { get; set; }              //Anciennitet
+        [DataMember]
+        public int YearsWithoutCrash { get; set; }
+        [DataMember]
+        public int MyProperty { get; set; }
+    }
+}
